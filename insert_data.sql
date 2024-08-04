dad9993f52eac1f712c7f7f85bc37057c3ec39e9
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2024-08-04 03:50:45 +0300

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,9 @@
+-- Insert into users table
+INSERT INTO users (username, password, email)
+VALUES ('testuser', 'testpassword', 'testuser@example.com');
+
+-- Insert into items table
+INSERT INTO items (name, description, price)
+VALUES ('Item1', 'This is item 1', 10.00),
+       ('Item2', 'This is item 2', 20.00),
+       ('Item3', 'This is item 3', 30.00);
